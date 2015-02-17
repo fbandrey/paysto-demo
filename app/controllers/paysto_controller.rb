f9e4d1b4a63ce0faafa49ce503d596ea6eb15609
Andrey B <fbandrey@gmail.com>
2015-02-17 16:05:35 +0300

    test IPs

--- app/controllers/paysto_controller.rb
+++ app/controllers/paysto_controller.rb
@@ -1,3 +1,16 @@
 class PaystoController < ActionController::Base
   include Paysto::Controller
+
+  def check
+    invoice = Paysto.invoice_class.find_by_id(params['PAYSTO_INVOICE_ID'])
+    puts "=======================> IP: #{request.remote_ip.inspect}"
+    if Paysto.invoice_valid?(invoice) && Paysto.ip_valid?(request.remote_ip) && Paysto.md5_valid?(params)
+      puts "====> Verification: true"
+      render text: invoice.id
+    else
+      puts "====> Verification: false"
+      render text: I18n.t('paysto.check.fail')
+    end
+  end
+
 end
